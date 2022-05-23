7fc1ee7591898542e0ed1840faaf1a1d516d726a
abdussalam-mustapha <103737038+abdussalam-mustapha@users.noreply.github.com>
2022-05-22 18:52:54 -0700

    Add files via upload

---
+++ CDE.js
@@ -0,0 +1,20 @@
+let boy ="john"
+if (boy == "joey"){
+     console.log("my name is" + boy)
+}else{
+    console.log("wrong")
+}
+
+
+    let count = 0;
+    let result = 0;
+
+    while(count < 10){
+        console.log(result)
+        result = result + 6;
+        count += count;
+    }
+
+
+
+
